d0078c4bb5e5c1342dcc9253eee253baed6676c4
Pau Gargallo <pau.gargallo@gmail.com>
2015-09-07 23:05:14 +0200

    Formatting

--- python/pyopengv.cpp
+++ python/pyopengv.cpp
@@ -40,7 +40,6 @@ opengv::bearingVector_t bearingVectorFromArray(
   return v;
 }
 
-
 opengv::point_t pointFromArray(
     const pyarray_t &array,
     size_t index )
@@ -52,13 +51,11 @@ opengv::point_t pointFromArray(
   return p;
 }
 
-
 bp::object arrayFromTranslation( const opengv::translation_t &t )
 {
   npy_intp shape[1] = {3};
   return bpn_array_from_data(1, shape, t.data());
 }
-
 
 bp::object arrayFromTransformation( const opengv::transformation_t &t )
 {
@@ -66,7 +63,6 @@ bp::object arrayFromTransformation( const opengv::transformation_t &t )
   npy_intp shape[2] = {3, 4};
   return bpn_array_from_data(2, shape, t_row_major.data());
 }
-
 
 bp::list listFromTransformations( const opengv::transformations_t &t )
 {
@@ -80,166 +76,165 @@ bp::list listFromTransformations( const opengv::transformations_t &t )
 
 
 namespace absolute_pose {
-  using namespace pyopengv;
 
-  class CentralAbsoluteAdapter : public opengv::absolute_pose::AbsoluteAdapterBase
+class CentralAbsoluteAdapter : public opengv::absolute_pose::AbsoluteAdapterBase
+{
+protected:
+  using AbsoluteAdapterBase::_t;
+  using AbsoluteAdapterBase::_R;
+
+public:
+  EIGEN_MAKE_ALIGNED_OPERATOR_NEW
+
+  /**
+   * \brief Constructor. See protected class-members to understand parameters
+   */
+  CentralAbsoluteAdapter(
+      bpn::array & bearingVectors,
+      bpn::array & points )
+    : _bearingVectors(bearingVectors)
+    , _points(points)
+  {}
+
+  /**
+   * \brief Constructor. See protected class-members to understand parameters
+   */
+  CentralAbsoluteAdapter(
+      bpn::array & bearingVectors,
+      bpn::array & points,
+      bpn::array & R )
+    : _bearingVectors(bearingVectors)
+    , _points(points)
   {
-  protected:
-    using AbsoluteAdapterBase::_t;
-    using AbsoluteAdapterBase::_R;
-
-  public:
-    EIGEN_MAKE_ALIGNED_OPERATOR_NEW
-
-    /**
-     * \brief Constructor. See protected class-members to understand parameters
-     */
-    CentralAbsoluteAdapter(
-        bpn::array & bearingVectors,
-        bpn::array & points )
-      : _bearingVectors(bearingVectors)
-      , _points(points)
-    {}
-
-    /**
-     * \brief Constructor. See protected class-members to understand parameters
-     */
-    CentralAbsoluteAdapter(
-        bpn::array & bearingVectors,
-        bpn::array & points,
-        bpn::array & R )
-      : _bearingVectors(bearingVectors)
-      , _points(points)
-    {
-      pyarray_t R_view(R);
-      for (int i = 0; i < 3; ++i) {
-        for (int j = 0; j < 3; ++j) {
-          _R(i, j) = R_view.get(i, j);
-        }
+    pyarray_t R_view(R);
+    for (int i = 0; i < 3; ++i) {
+      for (int j = 0; j < 3; ++j) {
+        _R(i, j) = R_view.get(i, j);
       }
     }
+  }
 
-    /**
-     * \brief Constructor. See protected class-members to understand parameters
-     */
-    CentralAbsoluteAdapter(
-        bpn::array & bearingVectors,
-        bpn::array & points,
-        bpn::array & t,
-        bpn::array & R )
-      : _bearingVectors(bearingVectors)
-      , _points(points)
-    {
-      pyarray_t t_view(t);
-      for (int i = 0; i < 3; ++i) {
-        _t(i) = t_view.get(i);
+  /**
+   * \brief Constructor. See protected class-members to understand parameters
+   */
+  CentralAbsoluteAdapter(
+      bpn::array & bearingVectors,
+      bpn::array & points,
+      bpn::array & t,
+      bpn::array & R )
+    : _bearingVectors(bearingVectors)
+    , _points(points)
+  {
+    pyarray_t t_view(t);
+    for (int i = 0; i < 3; ++i) {
+      _t(i) = t_view.get(i);
+    }
+    pyarray_t R_view(R);
+    for (int i = 0; i < 3; ++i) {
+      for (int j = 0; j < 3; ++j) {
+        _R(i, j) = R_view.get(i, j);
       }
-      pyarray_t R_view(R);
-      for (int i = 0; i < 3; ++i) {
-        for (int j = 0; j < 3; ++j) {
-          _R(i, j) = R_view.get(i, j);
-        }
-      }
     }
-    /**
-     * Destructor
-     */
-    virtual ~CentralAbsoluteAdapter() {}
+  }
+  /**
+   * Destructor
+   */
+  virtual ~CentralAbsoluteAdapter() {}
 
-    //Access of correspondences
+  //Access of correspondences
 
-    /** See parent-class */
-    virtual opengv::bearingVector_t getBearingVector( size_t index ) const {
-      opengv::bearingVector_t b = bearingVectorFromArray(_bearingVectors, index);
-      return b;
-    }
-    /** See parent-class */
-    virtual double getWeight( size_t index ) const {
-      return 1.0;
-    }
-    /** See parent-class. Returns zero for this adapter. */
-    virtual opengv::translation_t getCamOffset( size_t index ) const {
-      return Eigen::Vector3d::Zero();
-    }
-    /** See parent-class Returns identity for this adapter. */
-    virtual opengv::rotation_t getCamRotation( size_t index ) const {
-      return opengv::rotation_t::Identity();
-    }
-    /** See parent-class */
-    virtual opengv::point_t getPoint( size_t index ) const {
-      return pointFromArray(_points, index);
-    }
-    /** See parent-class */
-    virtual size_t getNumberCorrespondences() const {
-      return _bearingVectors.shape(0);
-    }
-
-  protected:
-    /** Reference to the bearing-vectors expressed in the camera-frame */
-    pyarray_t _bearingVectors;
-    /** Reference to the points expressed in the world-frame. */
-    pyarray_t _points;
-  };
-
-
-
-  bp::object p2p( bpn::array &v, bpn::array &p, bpn::array &R )
-  {
-    CentralAbsoluteAdapter adapter( v, p, R );
-    return arrayFromTranslation(
-      opengv::absolute_pose::p2p( adapter, 0, 1 ));
+  /** See parent-class */
+  virtual opengv::bearingVector_t getBearingVector( size_t index ) const {
+    opengv::bearingVector_t b = bearingVectorFromArray(_bearingVectors, index);
+    return b;
+  }
+  /** See parent-class */
+  virtual double getWeight( size_t index ) const {
+    return 1.0;
+  }
+  /** See parent-class. Returns zero for this adapter. */
+  virtual opengv::translation_t getCamOffset( size_t index ) const {
+    return Eigen::Vector3d::Zero();
+  }
+  /** See parent-class Returns identity for this adapter. */
+  virtual opengv::rotation_t getCamRotation( size_t index ) const {
+    return opengv::rotation_t::Identity();
+  }
+  /** See parent-class */
+  virtual opengv::point_t getPoint( size_t index ) const {
+    return pointFromArray(_points, index);
+  }
+  /** See parent-class */
+  virtual size_t getNumberCorrespondences() const {
+    return _bearingVectors.shape(0);
   }
 
-  bp::object p3p_kneip( bpn::array &v, bpn::array &p )
-  {
-    CentralAbsoluteAdapter adapter( v, p );
-    return listFromTransformations(
-      opengv::absolute_pose::p3p_kneip( adapter, 0, 1, 2 ));
-  }
+protected:
+  /** Reference to the bearing-vectors expressed in the camera-frame */
+  pyarray_t _bearingVectors;
+  /** Reference to the points expressed in the world-frame. */
+  pyarray_t _points;
+};
 
-  bp::object p3p_gao( bpn::array &v, bpn::array &p )
-  {
-    CentralAbsoluteAdapter adapter( v, p );
-    return listFromTransformations(
-      opengv::absolute_pose::p3p_gao( adapter, 0, 1, 2 ));
-  }
 
-  bp::object gp3p( bpn::array &v, bpn::array &p )
-  {
-    CentralAbsoluteAdapter adapter( v, p );
-    return listFromTransformations(
-      opengv::absolute_pose::gp3p( adapter, 0, 1, 2 ));
-  }
 
-  bp::object epnp( bpn::array &v, bpn::array &p )
-  {
-    CentralAbsoluteAdapter adapter( v, p );
-    return arrayFromTransformation(
-      opengv::absolute_pose::epnp( adapter ));
-  }
+bp::object p2p( bpn::array &v, bpn::array &p, bpn::array &R )
+{
+  CentralAbsoluteAdapter adapter(v, p, R);
+  return arrayFromTranslation(
+    opengv::absolute_pose::p2p(adapter, 0, 1));
+}
 
-  bp::object gpnp( bpn::array &v, bpn::array &p )
-  {
-    CentralAbsoluteAdapter adapter( v, p );
-    return arrayFromTransformation(
-      opengv::absolute_pose::gpnp( adapter ));
-  }
+bp::object p3p_kneip( bpn::array &v, bpn::array &p )
+{
+  CentralAbsoluteAdapter adapter(v, p);
+  return listFromTransformations(
+    opengv::absolute_pose::p3p_kneip(adapter, 0, 1, 2));
+}
 
-  bp::object upnp( bpn::array &v, bpn::array &p )
-  {
-    CentralAbsoluteAdapter adapter( v, p );
-    return listFromTransformations(
-      opengv::absolute_pose::upnp( adapter ));
-  }
+bp::object p3p_gao( bpn::array &v, bpn::array &p )
+{
+  CentralAbsoluteAdapter adapter(v, p);
+  return listFromTransformations(
+    opengv::absolute_pose::p3p_gao(adapter, 0, 1, 2));
+}
 
-  bp::object optimize_nonlinear( bpn::array &v,
-                                 bpn::array &p,
-                                 bpn::array &t,
-                                 bpn::array &R )
-  {
-    CentralAbsoluteAdapter adapter( v, p, t, R );
-    return arrayFromTransformation(optimize_nonlinear( adapter ));
-  }
+bp::object gp3p( bpn::array &v, bpn::array &p )
+{
+  CentralAbsoluteAdapter adapter(v, p);
+  return listFromTransformations(
+    opengv::absolute_pose::gp3p(adapter, 0, 1, 2));
+}
+
+bp::object epnp( bpn::array &v, bpn::array &p )
+{
+  CentralAbsoluteAdapter adapter(v, p);
+  return arrayFromTransformation(
+    opengv::absolute_pose::epnp(adapter));
+}
+
+bp::object gpnp( bpn::array &v, bpn::array &p )
+{
+  CentralAbsoluteAdapter adapter(v, p);
+  return arrayFromTransformation(
+    opengv::absolute_pose::gpnp(adapter));
+}
+
+bp::object upnp( bpn::array &v, bpn::array &p )
+{
+  CentralAbsoluteAdapter adapter(v, p);
+  return listFromTransformations(
+    opengv::absolute_pose::upnp(adapter));
+}
+
+bp::object optimize_nonlinear( bpn::array &v,
+                               bpn::array &p,
+                               bpn::array &t,
+                               bpn::array &R )
+{
+  CentralAbsoluteAdapter adapter(v, p, t, R);
+  return arrayFromTransformation(optimize_nonlinear(adapter));
+}
 
 } // namespace absolute_pose
 
@@ -258,5 +253,7 @@ BOOST_PYTHON_MODULE(pyopengv) {
   def("absolute_pose_epnp", pyopengv::absolute_pose::epnp);
   def("absolute_pose_gpnp", pyopengv::absolute_pose::gpnp);
   def("absolute_pose_upnp", pyopengv::absolute_pose::upnp);
+  def("absolute_pose_optimize_nonlinear",
+      pyopengv::absolute_pose::optimize_nonlinear);
 
 }
