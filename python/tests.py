27cfeb2a3539ea0b6c45f77bc89496a542b7e5f3
Pau Gargallo <pau.gargallo@gmail.com>
2015-09-08 15:01:03 +0200

    Add python relative pose ransac test

--- python/tests.py
+++ python/tests.py
@@ -35,50 +35,6 @@ def addNoise( noiseLevel, cleanPoint ):
     return noisyPoint / np.linalg.norm(noisyPoint)
 
 
-def generateRandom2D2DCorrespondences(position1, rotation1,
-                                      position2, rotation2,
-                                      num_points,
-                                      noise, outlier_fraction):
-    min_depth = 4
-    max_depth = 8
-
-    # initialize point-cloud
-    gt = np.empty((num_points, 3))
-    for i in range(num_points):
-        gt[i] = generateRandomPoint(max_depth, min_depth)
-
-    bearing_vectors1 = np.empty((num_points, 3))
-    bearing_vectors2 = np.empty((num_points, 3))
-    for i in range(num_points):
-        # get the point in viewpoint 1
-        body_point1 = rotation1.T.dot(gt[i] - position1)
-
-        # get the point in viewpoint 2
-        body_point2 = rotation2.T.dot(gt[i] - position2)
-
-        bearing_vectors1[i] = body_point1 / np.linalg.norm(body_point1)
-        bearing_vectors2[i] = body_point2 / np.linalg.norm(body_point2)
-
-        # add noise
-        if noise > 0.0:
-            bearing_vectors1[i] = addNoise(noise, bearing_vectors1[i])
-            bearing_vectors2[i] = addNoise(noise, bearing_vectors2[i])
-
-    # add outliers
-    num_outliers = int(outlier_fraction * num_points)
-    for i in range(num_outliers):
-        # create random point
-        p = generateRandomPoint(max_depth, min_depth)
-
-        # project this point into viewpoint 2
-        body_point = rotation2.T.dot(p - position2)
-
-        # normalize the bearing vector
-        bearing_vectors2[i] = body_point / np.linalg.norm(body_point)
-
-    return bearing_vectors1, bearing_vectors2
-
-
 def extractRelativePose(position1, position2, rotation1, rotation2):
     relativeRotation = rotation1.T.dot(rotation2)
     relativePosition = rotation1.T.dot(position2 - position1)
@@ -120,55 +76,120 @@ def matrix_in_list(a, l):
     return False
 
 
-def test_relative_pose_ransac():
+def same_transformation(position, rotation, transformation):
+    R = transformation[:, :3]
+    t = transformation[:, 3]
+    return proportional(position, t) and proportional(rotation, R)
+
+
+
+class RelativePoseDataset:
+    def __init__(self, num_points, noise, outlier_fraction):
+        # generate a random pose for viewpoint 1
+        position1 = np.zeros(3)
+        rotation1 = np.eye(3)
+
+        # generate a random pose for viewpoint 2
+        position2 = generateRandomTranslation(2.0)
+        rotation2 = generateRandomRotation(0.5)
+
+        # derive correspondences based on random point-cloud
+        self.bearing_vectors1, self.bearing_vectors2 = self.generateCorrespondences(
+            position1, rotation1, position2, rotation2,
+            num_points, noise, outlier_fraction)
+
+        # Extract the relative pose
+        self.position, self.rotation = extractRelativePose(
+            position1, position2, rotation1, rotation2)
+        self.essential = essentialMatrix(self.position, self.rotation)
+
+
+    def generateCorrespondences(self,
+                                position1, rotation1,
+                                position2, rotation2,
+                                num_points,
+                                noise, outlier_fraction):
+        min_depth = 4
+        max_depth = 8
+
+        # initialize point-cloud
+        gt = np.empty((num_points, 3))
+        for i in range(num_points):
+            gt[i] = generateRandomPoint(max_depth, min_depth)
+
+        bearing_vectors1 = np.empty((num_points, 3))
+        bearing_vectors2 = np.empty((num_points, 3))
+        for i in range(num_points):
+            # get the point in viewpoint 1
+            body_point1 = rotation1.T.dot(gt[i] - position1)
+
+            # get the point in viewpoint 2
+            body_point2 = rotation2.T.dot(gt[i] - position2)
+
+            bearing_vectors1[i] = body_point1 / np.linalg.norm(body_point1)
+            bearing_vectors2[i] = body_point2 / np.linalg.norm(body_point2)
+
+            # add noise
+            if noise > 0.0:
+                bearing_vectors1[i] = addNoise(noise, bearing_vectors1[i])
+                bearing_vectors2[i] = addNoise(noise, bearing_vectors2[i])
+
+        # add outliers
+        num_outliers = int(outlier_fraction * num_points)
+        for i in range(num_outliers):
+            # create random point
+            p = generateRandomPoint(max_depth, min_depth)
+
+            # project this point into viewpoint 2
+            body_point = rotation2.T.dot(p - position2)
+
+            # normalize the bearing vector
+            bearing_vectors2[i] = body_point / np.linalg.norm(body_point)
+
+        return bearing_vectors1, bearing_vectors2
+
+
+def test_relative_pose():
     print "Testing relative pose"
 
     # set experiment parameters
-    noise = 0.0
-    outlier_fraction = 0.0
-    num_points = 10
-
-    # generate a random pose for viewpoint 1
-    position1 = np.zeros(3)
-    rotation1 = np.eye(3)
-
-    # generate a random pose for viewpoint 2
-    position2 = generateRandomTranslation(2.0)
-    rotation2 = generateRandomRotation(0.5)
-
-    # derive correspondences based on random point-cloud
-    bearing_vectors1, bearing_vectors2 = generateRandom2D2DCorrespondences(
-        position1, rotation1, position2, rotation2,
-        num_points, noise, outlier_fraction)
-
-    # Extract the relative pose
-    position, rotation = extractRelativePose(
-        position1, position2, rotation1, rotation2)
-    essential = essentialMatrix(position, rotation)
+    d = RelativePoseDataset(10, 0.0, 0.0)
 
     # running experiments
-    twopt_translation = pyopengv.relative_pose_twopt(bearing_vectors1, bearing_vectors2, rotation)
-    fivept_nister_essentials = pyopengv.relative_pose_fivept_nister(bearing_vectors1, bearing_vectors2)
-    fivept_kneip_rotations = pyopengv.relative_pose_fivept_kneip(bearing_vectors1, bearing_vectors2)
-    sevenpt_essentials = pyopengv.relative_pose_sevenpt(bearing_vectors1, bearing_vectors2)
-    eightpt_essential = pyopengv.relative_pose_eightpt(bearing_vectors1, bearing_vectors2)
-    t_perturbed, R_perturbed = getPerturbedPose( position, rotation, 0.01)
-    eigensolver_rotation = pyopengv.relative_pose_eigensolver(bearing_vectors1, bearing_vectors2, R_perturbed)
-    t_perturbed, R_perturbed = getPerturbedPose( position, rotation, 0.1)
-    nonlinear_transformation = pyopengv.relative_pose_optimize_nonlinear(bearing_vectors1, bearing_vectors2, t_perturbed, R_perturbed)
+    twopt_translation = pyopengv.relative_pose_twopt(d.bearing_vectors1, d.bearing_vectors2, d.rotation)
+    fivept_nister_essentials = pyopengv.relative_pose_fivept_nister(d.bearing_vectors1, d.bearing_vectors2)
+    fivept_kneip_rotations = pyopengv.relative_pose_fivept_kneip(d.bearing_vectors1, d.bearing_vectors2)
+    sevenpt_essentials = pyopengv.relative_pose_sevenpt(d.bearing_vectors1, d.bearing_vectors2)
+    eightpt_essential = pyopengv.relative_pose_eightpt(d.bearing_vectors1, d.bearing_vectors2)
+    t_perturbed, R_perturbed = getPerturbedPose( d.position, d.rotation, 0.01)
+    eigensolver_rotation = pyopengv.relative_pose_eigensolver(d.bearing_vectors1, d.bearing_vectors2, R_perturbed)
+    t_perturbed, R_perturbed = getPerturbedPose( d.position, d.rotation, 0.1)
+    nonlinear_transformation = pyopengv.relative_pose_optimize_nonlinear(d.bearing_vectors1, d.bearing_vectors2, t_perturbed, R_perturbed)
 
-    assert proportional(position, twopt_translation)
-    assert matrix_in_list(essential, fivept_nister_essentials)
-    assert matrix_in_list(rotation, fivept_kneip_rotations)
-    assert matrix_in_list(essential, sevenpt_essentials)
-    assert proportional(essential, eightpt_essential)
-    assert proportional(rotation, eigensolver_rotation)
-    R = nonlinear_transformation[:,:3]
-    t = nonlinear_transformation[:,3]
-    assert proportional(position, t)
-    assert proportional(rotation, R)
+    assert proportional(d.position, twopt_translation)
+    assert matrix_in_list(d.essential, fivept_nister_essentials)
+    assert matrix_in_list(d.rotation, fivept_kneip_rotations)
+    assert matrix_in_list(d.essential, sevenpt_essentials)
+    assert proportional(d.essential, eightpt_essential)
+    assert proportional(d.rotation, eigensolver_rotation)
+    assert same_transformation(d.position, d.rotation, nonlinear_transformation)
 
     print "Done testing relative pose"
 
 
-test_relative_pose_ransac()
+def test_relative_pose_ransac():
+    print "Testing relative pose ransac"
+    # set experiment parameters
+    d = RelativePoseDataset(100, 0.0, 0.3)
+
+    ransac_transformation = pyopengv.relative_pose_ransac(
+        d.bearing_vectors1, d.bearing_vectors2, "NISTER", 0.01, 1000)
+
+    assert same_transformation(d.position, d.rotation, ransac_transformation)
+
+    print "Done testing relative pose ransac"
+
+
+if __name__ == "__main__":
+    test_relative_pose()
+    test_relative_pose_ransac()
